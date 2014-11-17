537c1a9e8a11e0f3bbb7eb74c9e8a58091cfa4f4
ca0abinary <jondemarks@gmail.com>
2014-11-16 22:42:12 -0500

    Added categories & sub-categories!

--- addon.py
+++ addon.py
@@ -1,17 +1,58 @@
-import urllib,urllib2,xbmcplugin,xbmcgui,sys
+import urllib,urllib2,urlparse
+import xbmcplugin,xbmcgui,sys
 import simplejson as json
 
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://mediator.jw.org/v1/categories/E/LatestVideos?&detailed=1'
-data = urllib2.urlopen(url).read().decode("utf-8")
-info = json.loads(data)
-results = info['category']['media']
+def build_url(query):
+	return base_url + '?' + urllib.urlencode(query)
 
-for r in results:
-	li = xbmcgui.ListItem(r.get('title'), iconImage=r['images']['pnr'].get('md'))
-	xbmcplugin.addDirectoryItem(handle=addon_handle, url=r['files'][-1].get('progressiveDownloadURL'), listitem=li)
+def build_folders(subcat_ary):
+	for s in subcat_ary:
+		url = build_url({'mode': s.get('key')})
+		li = xbmcgui.ListItem(s.get('name'))
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+		if 'media' in s: build_media_entries(s['media'])
 
-xbmcplugin.endOfDirectory(addon_handle)
+def build_media_entries(file_ary):
+	for r in file_ary:
+		if 'pnr' in r['images']: wide_img = r['images']['pnr'].get('md')
+		else: wide_img = ''
+
+		if 'sqr' in r['images']: img = r['images']['sqr'].get('md')
+		else: img = ''
+
+		li = xbmcgui.ListItem(r.get('title'))
+		li.setIconImage(wide_img)
+		li.setThumbnailImage(img)
+		li.setProperty('fanart_image', wide_img)
+		xbmcplugin.addDirectoryItem(handle=addon_handle,
+									url=r['files'][-1].get('progressiveDownloadURL'),
+									listitem=li)
+
+mode = args.get('mode', None)
+
+if mode is None:
+	cats_raw = urllib2.urlopen("http://mediator.jw.org/v1/categories/E?").read().decode("utf-8")
+	categories = json.loads(cats_raw)
+
+	for c in categories['categories']:
+		url = build_url({'mode': c.get('key')})
+		li = xbmcgui.ListItem(c.get('name'))
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+
+	xbmcplugin.endOfDirectory(addon_handle)
+
+else:
+	url = 'http://mediator.jw.org/v1/categories/E/' + mode[0] + '?&detailed=1'
+	print '==> Entering ' + url
+	data = urllib2.urlopen(url).read().decode("utf-8")
+	info = json.loads(data)
+	if 'subcategories' in info['category']: build_folders(info['category']['subcategories'])
+	if 'media' in info['category']: build_media_entries(info['category']['media'])
+
+	xbmcplugin.endOfDirectory(addon_handle)
