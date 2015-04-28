36c63f1195b93f186a99ce209e53d1ca639516fe
ca0abinary <jondemarks@gmail.com>
2015-04-28 19:56:38 -0400

    Fixed pictures and streaming

--- addon.py
+++ addon.py
@@ -15,10 +15,16 @@ def build_url(query):
 	return base_url + '?' + urllib.urlencode(query)
 
 def build_folders(subcat_ary):
+	isStreaming = mode[0] == 'Streaming'
 	for s in subcat_ary:
 		url = build_url({'mode': s.get('key')})
 		li = xbmcgui.ListItem(s.get('name'))
-		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True)
+		if 'rph' in s['images']:
+			li.setIconImage(s['images']['rph'].get('md'))
+			li.setThumbnailImage(s['images']['rph'].get('md'))
+		if 'pnr' in s['images']:
+			li.setProperty('fanart_image', s['images']['pnr'].get('md'))
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=(isStreaming == False))
 
 def get_video_metadata(file_ary):
 	videoFiles = []
@@ -26,6 +32,7 @@ def get_video_metadata(file_ary):
 		sqr_img = ''
 		wide_img = ''
 		if 'sqr' in r['images']: sqr_img = r['images']['sqr'].get('md')
+		elif 'cvr' in r['images']: sqr_img = r['images']['cvr'].get('md')
 		if 'pnr' in r['images']: wide_img = r['images']['pnr'].get('md')
 		video = sorted([x for x in r['files'] if x['frameHeight'] <= video_res], reverse=True)[0]
 		videoFiles.append({'id':r['_id'],'video':video['progressiveDownloadURL'],'wide_img':wide_img,'sqr_img':sqr_img,'title':r.get('title'),'dur':r.get('duration')})
@@ -91,14 +98,32 @@ def process_sub_level(sub_level, create_playlist, from_id):
 	info = json.loads(data)
 	if create_playlist == False:
 		if 'subcategories' in info['category']: build_folders(info['category']['subcategories'])
-		if 'media' in info['category']: build_media_entries(info['category']['media'])
+		elif 'media' in info['category']: build_media_entries(info['category']['media'])
 		xbmcplugin.endOfDirectory(addon_handle)
 	else:
 		pl = build_playlist(info['category']['media'], from_id)
 		xbmc.Player().play(pl)
 
+def process_streaming():
+	print 'entering process_streaming for ' + mode[0]
+	url = 'http://mediator.jw.org/v1/schedules/E/Streaming'
+	data = urllib2.urlopen(url).read().decode("utf-8")
+	info = json.loads(data)
+	for s in info['category']['subcategories']:
+		if s['key'] == mode[0]:
+			pl = xbmc.PlayList(1)
+			pl.clear()
+			for item in get_video_metadata(s['media']):
+				li = xbmcgui.ListItem(item['title'], iconImage=item['sqr_img'])
+				li.setThumbnailImage(item['sqr_img'])
+				pl.add(item['video'], li)
+			xbmc.Player().play(pl)
+			xbmc.Player().seekTime(s['position']['time'])
+			return
+
 mode = args.get('mode', None)
 
 if mode is None: process_top_level()
 elif mode[0] == 'watch_from_here': process_sub_level(args.get('from_mode')[0], True, args.get('first')[0])
+elif (mode[0].startswith('Streaming') and len(mode[0]) > 9): process_streaming()
 else: process_sub_level(mode[0], False, 0)
